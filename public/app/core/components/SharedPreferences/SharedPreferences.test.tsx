43a6eee2f8edfbef8344b414c1f460a221c6fd15
MattIPv4 <me@mattcowley.co.uk>
2026-07-22 22:53:01 +0100

    Enable unified homepage for all users (remove flag)

--- public/app/core/components/SharedPreferences/SharedPreferences.test.tsx
+++ public/app/core/components/SharedPreferences/SharedPreferences.test.tsx
@@ -214,7 +214,6 @@ describe('SharedPreferences', () => {
       expect(jest.mocked(homeDashboardChanged)).toHaveBeenCalledWith({
         preferenceType: 'user',
         action: 'set',
-        unifiedHomepageEnabled: false,
       });
     });
   });
